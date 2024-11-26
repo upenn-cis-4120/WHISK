f81598513475d7763ad8fd2eaf5e7349ff8e7c2c
logbrass <brassingtonlogan@gmail.com>
2024-11-26 14:27:47 -0500

    quick shift

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,3 +1,44 @@
+// import React from "react";
+// import PropTypes from "prop-types";
+// import "./EventCard.css";
+
+// function EventCard({ event }) {
+//   return (
+//     <div className="event-card">
+//       <h2 className="date-header">{event.date}</h2>
+//       <div className="stats-container">
+//         <div className="stat-box guests">
+//           <span className="stat-number">{event.stats.guests}</span>
+//           <span className="stat-label">Guests</span>
+//         </div>
+//         <div className="stat-box outstanding">
+//           <span className="stat-number">${event.stats.outstanding}</span>
+//           <span className="stat-label">Outstanding</span>
+//         </div>
+//         <div className="stat-box alerts">
+//           <span className="stat-number">{event.stats.alerts}</span>
+//           <span className="stat-label">Alerts</span>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// EventCard.propTypes = {
+//   event: PropTypes.shape({
+//     date: PropTypes.string.isRequired,
+//     stats: PropTypes.shape({
+//       guests: PropTypes.number.isRequired,
+//       outstanding: PropTypes.number.isRequired,
+//       alerts: PropTypes.number.isRequired
+//     }).isRequired
+//   }).isRequired
+// };
+
+// export default EventCard;
+
+
+
 import PropTypes from "prop-types";
 import "./EventCard.css";
 
