86dc4be5de5e0cdd062053d5d024e1ded5955a05
Vedansh Goenka <vedanshg@seas.upenn.edu>
2024-11-30 21:35:07 -0500

    Update header to be clickable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleHeaderClick = () => {
+    navigate("/");
+  };
+
   return (
-    <header className="header">
+    <div className="header" onClick={handleHeaderClick} style={{ cursor: "pointer" }}>
       <div className="logo-container">
         <div className="whisk-text">WHISK</div>
         <div className="whisk-handle"></div>
@@ -15,7 +22,7 @@ function Header() {
         <div className="whisk-underline line5"></div>
         <div className="whisk-underline line6"></div>
       </div>
-    </header>
+    </div>
   );
 }
 
